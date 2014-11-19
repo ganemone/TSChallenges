928f5265eff2d6d2fbff9b6a3418cd8091fabd9b
Giancarlo Anemone <ganemone@gmail.com>
2014-11-18 19:25:56 -0500

    Add tests

--- src/__tests__/CoolString-reverse-in-place.ts
+++ src/__tests__/CoolString-reverse-in-place.ts
@@ -16,27 +16,27 @@ jest.dontMock('../CoolString.js');
 describe('CoolString reverse function', () => {
     it('should reverse a 1 character string correctly', () => {
         var s: CoolString = new CoolString('a');
-        s.reverse();
+        s.reverseInPlace();
         assert.equal(s.data, 'a');
     });
     it('should reverse a 2 character string correctly', () => {
         var s: CoolString = new CoolString('ab');
-        s.reverse();
+        s.reverseInPlace();
         assert.equal(s.data, 'ba');
     });
     it('should reverse a 3 character string correctly', () => {
         var s: CoolString = new CoolString('abc');
-        s.reverse();
+        s.reverseInPlace();
         assert.equal(s.data, 'cba');
     });
     it('should reverse a 4 character string correctly', () => {
         var s: CoolString = new CoolString('abcd');
-        s.reverse();
+        s.reverseInPlace();
         assert.equal(s.data, 'dcba');
     });
     it('should reverse a 5 character string correctly', () => {
         var s: CoolString = new CoolString('abcde');
-        s.reverse();
+        s.reverseInPlace();
         assert.equal(s.data, 'edcba');
     });
 });
